92ca624576da606a692e4022e00f1d4b7fa849f0
joanavrsilva <joanavrsilva@gmail.com>
2021-09-11 16:26:36 +0000

    Add pop-up msgm and image to level 1 and level2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-const handOptions = {
+  
+  //Start -- Pop-up getting elemenets & necessary functions
+  
+  //Get necessary Pop-up elements
+ // Get the Pop-up
+  var gameOverPopUp = document.getElementById("gameOverPopUp");
+
+  // Get the <span> element that redirects the player home
+  var goHomeLink = document.getElementsByClassName("goHome")[0];
+
+  // Get Image & Title you Lose
+  var youLoseImg = document.getElementById("youLoseImg");
+  var youLoseTit = document.getElementById("youLoseTit");
+
+  // Get Image & Title you Win
+  var youWinImg = document.getElementById("youWinImg");
+  var youWinTit = document.getElementById("youWinTit");
+  
+  // Go Home function
+  goHomeLink.onclick = function() {
+    gameOverPopUp.style.display = "none";
+    window.location.href = "index.html";
+  }
+
+  //Stat -- Play & Score Functions
+  const handOptions = {
     'rock': "assets/images/Rock.png",
     'paper': "assets/images/Paper.png",
     'scissors': "assets/images/Scissors.png",
@@ -9,7 +34,7 @@ const handOptions = {
   let SCORE = 0;
   let SCORECP = 0;
   
-  const pickUserHand = (hand) => {
+  const playUserHand = (hand, Level) => {
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
   
@@ -19,18 +44,37 @@ const handOptions = {
     // set user Image
     document.getElementById("userPickImage").src = handOptions[hand];
   
-    pickComputerHand(hand);
+    //Select function depending on the Level to choose CpHand randomly
+    if (Level==1){
+      cpHand = pickComputerHandL1();
+    } else{
+      cpHand = pickComputerHandL2();
+    }
+
+    referee(hand, cpHand);
+
   };
   
-  const pickComputerHand = (hand) => {
-      let hands = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+  const pickComputerHandL1 = () => {
+      let hands = ['rock', 'paper', 'scissors'];
       let cpHand = hands[Math.floor(Math.random() * hands.length)];
       
       // set computer image 
-      document.getElementById("computerPickImage").src = handOptions[cpHand]
+      document.getElementById("computerPickImage").src = handOptions[cpHand];
       
-      referee(hand, cpHand);
+      return cpHand;
   };
+
+  const pickComputerHandL2 = () => {
+    let hands = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+    let cpHand = hands[Math.floor(Math.random() * hands.length)];
+    
+    // set computer image 
+    document.getElementById("computerPickImage").src = handOptions[cpHand];
+      
+    return cpHand;  
+  };
+
   // New code
   const handWins = {
     'rock': ['scissors', 'lizard'],
@@ -50,11 +94,30 @@ const handOptions = {
       setDecision("YOU WIN!");
       SCORE=SCORE + 1
       setScore(SCORE,"scoreUser");
+      /*Put this in a dedicated function*/
+      
+      if(SCORE==3){
+        /*Put this in a dedicated function*/
+        gameOverPopUp.style.display = "block";
+        youWinImg.style.display = "block";
+        youWinTit.style.display = "block";
+     }
+
     }
     else {
       setDecision("YOU LOSE!");
       SCORECP=SCORECP + 1
       setScore(SCORECP,"scoreCP");
+
+     
+      if(SCORECP==3){
+         /*Put this in a dedicated function*/
+        gameOverPopUp.style.display = "block";
+        youLoseImg.style.display = "block";
+        youLoseTit.style.display = "block";
+      }
+      
+
     }
     
 }
@@ -74,3 +137,4 @@ const handOptions = {
   const setScore = (newScore, id) => {
     document.getElementById(id).innerText = newScore;
   }
+//End -- Play & Score Functions
